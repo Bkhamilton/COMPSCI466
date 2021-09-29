42a28846e2316efe9eaae9fabfd85d4dad8a578b
Bkhamilton <43786988+Bkhamilton@users.noreply.github.com>
2021-09-29 18:52:50 -0400

    Update PageRank.py

--- Project2/PageRank.py
+++ Project2/PageRank.py
@@ -1,7 +1,9 @@
 #Ben Hamilton
 #Project 2
 
-#Start with Pseudocode
+#Graph G has vertices, V, and edges, E
+#vertices = Pages, edges = Links
+
 def pageRank(G):
   pages = G.vertices
   links = G.edges
