683c536cc14fae3cb66418cfe14342b70215bc8e
Bkhamilton <43786988+Bkhamilton@users.noreply.github.com>
2021-09-30 14:38:07 -0400

    Update PageRank.java

--- Project2/PageRank.java
+++ Project2/PageRank.java
@@ -1,5 +1,25 @@
+import java.util.*;
+
+public class Graph {
+  String[] pages;
+  String[] links;
+}
+
 public class pageRank {
+  
+  static final double lambda = 0.15;
+  static final double tau = 0.0001;
+  
+  
+  
+  public static double PageRank(Graph G) {
+      
+  }
+  
   public static void main(String[] args) {
+    Map<String, Double> map = new HashMap<String, Double>();
+    
+    
     System.out.println("Hello");
   }
 }
